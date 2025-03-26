4b7cfc2d66fe412397fee024cb2a17eea83d1350
mspr <mspr9773@gmail.com>
2025-03-26 18:39:22 +0530

    first commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,21 @@
-resource "aws_lambda_function" "ec2_stopped_mail_lambda_function" {
+resource "aws_lambda_function" "function1" {
   function_name = "ec2_stopped_mail_lambda_function"
   runtime       = "python3.9"
   role          = aws_iam_role.lambda_role.arn
   handler       = "lambda_function.lambda_handler"
-  filename      = "ec2_stopped_mail_lambda_function.zip"
+  filename      = "function1.zip"
 }
 
-resource "aws_lambda_function" "ebs_backup_mail_lambda_function" {
+resource "aws_lambda_function" "function2" {
   function_name = "ebs_backup_mail_lambda_function"
   runtime       = "python3.9"
   role          = aws_iam_role.lambda_role.arn
   handler       = "lambda_function.lambda_handler"
-  filename      = "ebs_backup_mail_lambda_function.zip"
+  filename      = "function2.zip"
 }
 
 resource "aws_lambda_function" "function3" {
-  function_name = "function3"
+  function_name = "function3Convert_ebs_volume-from-gp2-to-gp3_lambda_function"
   runtime       = "python3.9"
   role          = aws_iam_role.lambda_role.arn
   handler       = "lambda_function.lambda_handler"
